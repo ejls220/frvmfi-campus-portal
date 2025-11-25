48cc53e0929c2bc3a893921b203ccef883a8b718
ejls220 <boytagok083@gmail.com>
2025-11-25 14:26:44 +0800

    Change QR code save path to current directory
    Updated QR code generation to save in the current directory instead of 'static' folder.

--- qr_generator.py
+++ qr_generator.py
@@ -1,8 +1,10 @@
 import qrcode
 import os
 
+# GitHub Pages URL to your map.html
 url = "https://ejls220.github.io/frvmfi-campus-portal/map.html"
 
+# Create QR code
 qr = qrcode.QRCode(
     version=1,
     box_size=10,
@@ -11,10 +13,8 @@ qr = qrcode.QRCode(
 qr.add_data(url)
 qr.make(fit=True)
 
-if not os.path.exists("static"):
-    os.makedirs("static")
-
-img_path = os.path.join("static", "qrcode.png")
+# Save QR code in the same folder as HTML
+img_path = "qrcode.png"
 img = qr.make_image(fill="black", back_color="white")
 img.save(img_path)
 
